4b5050cd8d2f85daedd8ed2274a6035659daf54c
Jeffrey Wang <jeffrey.19jw@gmail.com>
2025-09-22 13:32:41 -0400

    feat: Implement map display and navigation
    This commit introduces functionality to display user-created maps and navigate to specific locations.
    
    Key changes:
    - Added `DisplayMapActivity` to show selected maps with markers.
    - Implemented `MapsAdapter` for the RecyclerView in `MainActivity` to list user maps.
    - Created data models `Place` and `UserMap` to represent map data.
    - Added layout for `DisplayMapActivity`.

---
+++ app/src/main/java/com/example/mymaps/DisplayMapActivity.kt
@@ -0,0 +1,75 @@
+package com.example.mymaps
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.mymaps.databinding.ActivityDisplayMapBinding
+import com.example.mymaps.models.UserMap
+import com.google.android.gms.maps.model.LatLngBounds
+
+class DisplayMapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityDisplayMapBinding
+
+    private lateinit var userMap: UserMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDisplayMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) { // TIRAMISU is API 33
+            userMap = intent.getSerializableExtra(EXTRA_USER_MAP, UserMap::class.java)!!
+        } else {
+            @Suppress("DEPRECATION") // Suppress warning for the older API call
+            userMap = (intent.getSerializableExtra(EXTRA_USER_MAP) as? UserMap)!!
+        }
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        print("user map to render: ${userMap.title}")
+        // Add map markers from user map
+        // bound builder for moving camera to fit all markers in bounds
+        val boundsBuilder = LatLngBounds.Builder()
+        for (place in userMap.places) {
+            val latLng = LatLng(place.latitude, place.longitude)
+            boundsBuilder.include(latLng)
+            mMap.addMarker(MarkerOptions().position(latLng).title(place.title).snippet(place.description))
+
+        }
+        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(boundsBuilder.build(), 1000, 1000, 10))
+
+
+
+//
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
---
+++ app/src/main/java/com/example/mymaps/MapsAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.mymaps
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mymaps.models.UserMap
+
+class MapsAdapter(val context: Context, val userMaps: List<UserMap>, val onClickListener: OnClickListener) : RecyclerView.Adapter<MapsAdapter.ViewHolder>() {
+    //implementation from the mainactivity file
+    interface OnClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+
+        val view =
+            LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int
+    ) {
+        //bind data to the view
+        val userMap = userMaps[position]
+        val textViewTitle = holder.itemView.findViewById<TextView>(android.R.id.text1)
+        textViewTitle.text = userMap.title
+
+        holder.itemView.setOnClickListener {
+            println("tapped on position $position")
+            onClickListener.onItemClick(position)
+        }
+
+    }
+
+    override fun getItemCount(): Int = userMaps.size
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
+
